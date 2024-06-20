a5684683d93e66b986a6de7a727185986c1d889d
Chris Bonifacio <christopher.bonifacio@gmail.com>
2024-06-20 11:26:59 -0400

    update home page to load photo submissions

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,55 @@
 "use client";
 
+import React from "react";
+
+import { signOut } from "aws-amplify/auth";
+import { list } from "aws-amplify/storage";
+import { StorageImage } from "@aws-amplify/ui-react-storage";
+
 export default function Home() {
+  const [imagePaths, setImagePaths] = React.useState<string[]>();
+
+  const getPictureSubmissions = async () => {
+    let paths;
+
+    const result = await list({
+      path: "picture-submissions/",
+    });
+
+    console.log(paths);
+
+    paths = result.items.map((item) => item.path);
+
+    setImagePaths(paths);
+  };
+
+  React.useEffect(() => {
+    getPictureSubmissions();
+  }, []);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-3xl">
-        Amplify Gen 2 - Files and Functions in 30 Minutes or Less!
-      </h1>
+      <h1 className="text-2xl mb-0">Amplify Gen 2 - Files and Functions</h1>
+      <h2>in 30 Minutes or Less!</h2>
+
+      <button
+        onClick={async () => {
+          await signOut();
+        }}
+      >
+        Sign Out
+      </button>
+
+      <h2>Images</h2>
+
+      {imagePaths?.map((path) => (
+        <StorageImage
+          key={path}
+          path={path}
+          alt="user submitted photo"
+          width={100}
+        />
+      ))}
     </main>
   );
 }
